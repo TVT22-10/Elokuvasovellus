d33dcbd14958463f9d0eac2d41d03055a3458ebb
nooruuw <nooramaria.ylitalo@gmail.com>
2023-11-14 14:39:35 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TopBar from './components/TopBar'; 
+import TopBar from './components/TopBar';
 import { Login } from './components/Auth';
 import { Register } from './components/Register';
+import Profile from "./pages/profile/my_profile/my_profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/Auth" element={<Login />} /> {/* Login page */}
         <Route path="/register" element={<Register />} /> {/* Registration page */}
+        <Route path="/profile" element={<Profile />} /> {/* Profile page */}
       </Routes>
     </Router>
   );
