9c20607fd1646572e46d6ef607485d059981cbb2
nooruuw <nooramaria.ylitalo@gmail.com>
2023-11-14 09:35:47 +0200

    profiilisivu

--- client/src/pages/start/start.js
+++ client/src/pages/start/start.js
@@ -1,14 +1,1 @@
 
-import React from 'react';
-
-function HomePage() {
-  return (
-    <div style={{ /* add any necessary styles here */ }}>
-      <h1>Welcome to My App!</h1>
-      <p>This is a brief description of my app.</p>
-      <button>Go to Another Page</button>
-    </div>
-  );
-}
-
-export default HomePage;
