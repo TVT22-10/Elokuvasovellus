7f6994b59ffc5307720f3fc4305bcc90c7e452c1
valtterivimpari <116632395+valtterivimpari@users.noreply.github.com>
2023-11-15 09:42:40 +0200

    Update edit_group.js

---
+++ client/src/pages/groups/edit_group/edit_group.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './edit_group.css'; // Import your stylesheet
+
+class EditGroup extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="top-bar">
+          <h1>Elokuvakerho</h1>
+          <nav>
+            <p>Etsi</p>
+            <p>Selaa</p>
+            <p>Ryhmät</p>
+            <p>Profiili</p>
+          </nav>
+        </div>
+        <hr className="separator" />
+
+        <div className="box-container">
+          <div className="box">Muokkaa ryhmää</div>
+          <div className="box">Tietoja</div>
+          <div className="box">Jäsenet</div>
+          <div className="box">Uutiset</div>
+        </div>
+        <div className="profile-container">
+          <img src="path/to/Unknown_person.jpg" alt="Profile Picture" />
+        </div>
+        <p className="sample-text">Sample Text</p>
+      </div>
+    );
+  }
+}
+
+export default EditGroup;
