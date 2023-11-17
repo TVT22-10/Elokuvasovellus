39a9ff2580300a4fa10b019741d6d1aba59ac38c
PohjanenJulius <julius.pohjanen@gmail.com>
2023-11-17 09:12:39 +0200

    sumting

--- client/src/App.js
+++ client/src/App.js
@@ -21,9 +21,8 @@ function App() {
         <Route path="/Auth" element={<Login />} /> {/* Login page */}
         <Route path="/register" element={<Register />} /> {/* Registration page */}
         <Route path="/profile" element={<Profile />} /> {/* Profile page */}
-        <Route path="/create_group" element={<CreateGroup />} /> {/* Profile page */}
-        <Route path="/edit_group" element={<EditGroup />} /> {/* Profile page */}
-        <Route path="/register" element={<Register />} /> {/* Profile page */}
+        <Route path="/create_group" element={<CreateGroup />} /> {/* Create Group page */}
+        <Route path="/edit_group" element={<EditGroup />} /> {/* edit group page */}
         <Route path="/browse_all" element={<BrowseAll />} /> {/* Profile page */}
         <Route path="/browse_reviews" element={<BrowseReviews />} /> {/* Profile page */}
       </Routes>
--- client/src/pages/groups/edit_group/edit_group.js
+++ client/src/pages/groups/edit_group/edit_group.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './edit_group.css'; // Import your stylesheet
+import './edit_group.css'; 
 
 function EditGroup() {
   return (
@@ -14,7 +14,6 @@ function EditGroup() {
         </nav>
       </div>
       <hr className="separator" />
-
       <div className="box-container">
         <div className="box">Muokkaa ryhmää</div>
         <div className="box">Tietoja</div>
