d3e50cd06d61f49fa3080eb90a891a460b08e25a
tompih <112546914+tompih@users.noreply.github.com>
2023-11-20 13:49:58 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import React, { useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TopBar from './components/TopBar';
 import { Login } from './components/Auth';
-import { AuthProvider } from './components/Contexts'; // Adjust the path if necessary
+import { AuthProvider, AuthContext } from './components/Contexts'; // Corrected import path
 import { Register } from './components/Register';
-import HomePage from './pages/start/start'; // Import HomePage
-import MovieDetail from './moviecomponents/MovieDetail'; // Adjust the path as per your directory structure
+import HomePage from './pages/start/start';
+import MovieDetail from './moviecomponents/MovieDetail';
 import Profile from "./pages/profile/my_profile/my_profile";
 import CreateGroup from './pages/groups/create_group/create_group';
 import EditGroup from './pages/groups/edit_group/edit_group';
@@ -12,10 +13,6 @@ import BrowseAll from './pages/browse/browse_all/browse_all';
 import BrowseReviews from './pages/browse/browse_reviews/browse_reviews';
 import Edit_Profile from './pages/profile/edit_profile/edit_profile';
 import SearchPage from './pages/search/search';
-import Edit_Profile from './pages/profile/edit_profile/edit_profile';
-import React, { useContext, useEffect } from 'react';
-
-import { AuthContext } from './components/Contexts'; // Adjust the path as necessary
 
 function RepeatingLogComponent() {
   const { isLoggedIn } = useContext(AuthContext);
@@ -33,29 +30,26 @@ function RepeatingLogComponent() {
 
 function App() {
   return (
-    <AuthProvider> {/* Wrap everything within AuthProvider */}
-          <RepeatingLogComponent /> {/* Add this line */}
+    <AuthProvider>
+      <RepeatingLogComponent />
 
-          <Router>
-       <TopBar />  {/*Render TopBar on all pages */}
-      <Routes>
-      <Route path="/" element={<HomePage />} /> {/* HomePage as the default route */}
-        <Route path="/movies/:movieId" element={<MovieDetail />} />
-        <Route path="/Auth" element={<Login />} /> {/* Login page */}
-        <Route path="/register" element={<Register />} /> {/* Registration page */}
-        <Route path="/profile" element={<Profile />} /> {/* Profile page */}
-        <Route path="/edit_profile" element={<Edit_Profile />} /> {/* Profile page */}
-        <Route path="/create_group" element={<CreateGroup />} /> {/* Create group page */}
-        <Route path="/edit_group/:groupId" element={<EditGroup />} /> {/* Edit group page */}
-        <Route path="/browse_all" element={<BrowseAll />} /> {/* Browse all page */}
-        <Route path="/browse_reviews" element={<BrowseReviews />} /> {/* Browse reviews page */}
-        <Route path="/edit_profile" element={<Edit_Profile />} /> {/* Edit profile page */}
-        <Route path="/search" element={<SearchPage />} /> {/* Search page */}
-
-      </Routes>
-    </Router>
+      <Router>
+        <TopBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies/:movieId" element={<MovieDetail />} />
+          <Route path="/Auth" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit_profile" element={<Edit_Profile />} />
+          <Route path="/create_group" element={<CreateGroup />} />
+          <Route path="/edit_group/:groupId" element={<EditGroup />} />
+          <Route path="/browse_all" element={<BrowseAll />} />
+          <Route path="/browse_reviews" element={<BrowseReviews />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </Router>
     </AuthProvider>
-
   );
 }
 
