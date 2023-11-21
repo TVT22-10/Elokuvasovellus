1b3d05f31b6411804823a77e2cc38eb2b1143d2a
nooruuw <nooramaria.ylitalo@gmail.com>
2023-11-21 14:33:53 +0200

    group-sivun muokkailua

--- client/src/pages/groups/create_group/create_group.js
+++ client/src/pages/groups/create_group/create_group.js
@@ -1,18 +1,62 @@
 import React from 'react';
 import './create_group.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 function CreateGroup() {
+
+  const [groupName, setGroupName] = useState('');
+  const [groupDescription, setGroupDescription] = useState('');
+
+  const handleDescriptionChange = (e) => {
+    if (e.key === 'Enter') {
+      setGroupDescription((prevDescription) => prevDescription + '\n');
+      e.preventDefault(); // Prevent the default Enter behavior
+    }
+  };
+
+
+  function CreateNewGroup(e) {
+    e.preventDefault();
+  }
+
+
   return (
     <div className="create_group">
-      <div className="label-container">
-        <h4>Hae ryhmiä</h4>
-        <input type="text" id="username" name="username" />
+      <div className="label-name">
+        <h2>Create group</h2>
       </div>
-      <h2>Omat ryhmäni</h2>
-      <div className="new-group">
-      <button id="new_group">Create new group</button>
-    </div>
+      <form onSubmit={CreateNewGroup}>
+        <div className="group_container">
+          <div className="group-name">
+          <label htmlFor="group_name">Group name:</label>
+            <input 
+            type="text" 
+            value={groupName} 
+            onChange={(e) => setGroupName(e.target.value)}
+            id="group_name"
+            name="group_name"
+            />
+          </div>
+          <div className="group-description">
+          <label htmlFor="group_description">Description:</label>
+          <textarea
+              id="group_description"
+              name="group_description"
+              rows={15} // Set the number of rows
+              value={groupDescription}
+              onChange={(e) => setGroupDescription(e.target.value)}
+              onKeyDown={handleDescriptionChange}
+            ></textarea>
+          </div>
+        </div>
+        <div className="new-group">
+          <Link to ="/group_page">
+          <button id="new_group">Create new group</button>
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
