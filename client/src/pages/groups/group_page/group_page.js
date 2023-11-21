81b5b0bd1dbe0f74c95cca7bda10dcc20312dbc3
nooruuw <nooramaria.ylitalo@gmail.com>
2023-11-21 15:04:37 +0200

    Update group_page.js

--- client/src/pages/groups/group_page/group_page.js
+++ client/src/pages/groups/group_page/group_page.js
@@ -6,8 +6,9 @@ import { Link } from 'react-router-dom';
 
 
 function GroupPage() {
+  const [activeTab, setActiveTab] = useState('description'); // State for active tab
   const [groupName, setGroupName] = useState(''); // State for the username
-  const [creationDate, setCreationDate] = useState('');
+  //const [creationDate, setCreationDate] = useState('');
 
 
 //   useEffect(() => {
@@ -53,28 +54,29 @@ return (
           </div>
           <div className="profile">
             <div className="username">
-              <h2>{username}</h2>
-              <p>Account Created On: {creationDate}</p>
+              <h2>(group name)</h2>
+              {/*<p>Account Created On: {creationDate}</p>*/}
             </div>
             <div className="bio-buttons">
             <div className="share-button">
-              <button id="edit" onClick={generateShareableLink}>Share the view</button>
+             {/* <button id="edit" onClick={generateShareableLink}>Share the view</button>*/}
             </div>
             <div className="edit-button">
             <Link to="/edit_group">
-          <button id="edit">Profile Settings</button>
+          <button id="edit">Group settings</button>
         </Link>
             </div>
           </div>
           </div>
         </div>
-        <div className="profile-buttons">
-          <p className={`view-change ${activeTab === 'favourites' ? 'active-link' : ''}`} onClick={() => setActiveTab('favourites')}>Favourites</p>
-          <p className={`view-change ${activeTab === 'reviews' ? 'active-link' : ''}`} onClick={() => setActiveTab('reviews')}>Reviews</p>
-          <p className={`view-change ${activeTab === 'posts' ? 'active-link' : ''}`} onClick={() => setActiveTab('posts')}>Posts</p>
+        <div className="Group-buttons">
+          <p className={`view-change ${activeTab === 'group settings' ? 'active-link' : ''}`} onClick={() => setActiveTab('group settings')}>Group settings</p>
+          <p className={`view-change ${activeTab === 'description' ? 'active-link' : ''}`} onClick={() => setActiveTab('description')}>Description</p>
+          <p className={`view-change ${activeTab === 'group members' ? 'active-link' : ''}`} onClick={() => setActiveTab('group members')}>Group members</p>
+          <p className={`view-change ${activeTab === 'news' ? 'active-link' : ''}`} onClick={() => setActiveTab('news')}>news</p>
         </div>
-        <div className="profile-content">
-          <div className={`content ${activeTab !== 'favourites' && 'hidden'}`} id="favourites">
+        <div className="Group-content">
+          <div className={`content ${activeTab !== 'group settings' && 'hidden'}`} id="favourites">
             <p>Tähän tulis sitten käyttäjän tykätyt elokuvat</p>
           </div>
           <div className={`content ${activeTab !== 'reviews' && 'hidden'}`} id="reviews">
