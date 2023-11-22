6d79a6d79f271875f28ffb85c532637b0c06eab6
nooruuw <nooramaria.ylitalo@gmail.com>
2023-11-22 10:59:34 +0200

    jotain

--- client/src/moviecomponents/MovieDetail.js
+++ client/src/moviecomponents/MovieDetail.js
@@ -4,7 +4,7 @@ import { AuthContext } from '../components/Contexts'; // Adjust the path as nece
 import axios from 'axios';
 import './MovieDetail.css';
 import './LoadingScreen.css';
-import MovieReviews from '../pages/movie_review/movie_review'; 
+
 
 function MovieDetail() {
   const { movieId } = useParams();
@@ -76,8 +76,6 @@ function MovieDetail() {
     console.log('Username and MovieID:', { username, movieId }); // Log relevant information
   
     try {
-      const response = await axios.post(`http://localhost:3001/favorites/${username}`, { movieId });
-      
       let response;
       if (isFavorite) {
         response = await axios.delete(`http://localhost:3001/favorites/${username}/remove/${movieId}`);
@@ -145,9 +143,6 @@ function MovieDetail() {
           <p>{movieDetails.overview}</p>
 
           {/* Add to Favorites button */}
-          {isLoggedIn && (
-            <button className="favorite-button" onClick={addToFavorites}>Add to Favorites</button>
-          )}
           <div className="favorite-button-container">
   {isLoggedIn && (
     <button className="favorite-button1" onClick={addToFavorites}>
@@ -179,9 +174,6 @@ function MovieDetail() {
             </div>
             <button onClick={() => scrollCast(300)} className="cast-scroll-button right-arrow">&gt;</button>
           </div>
-
-         {/* Reviews section */}
-       <MovieReviews movieId={movieId} />
         </div>
       </div>
     </div>
