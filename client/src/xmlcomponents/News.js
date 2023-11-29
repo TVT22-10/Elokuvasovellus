09a127dffc2e90ffb03996dddb88c72aaffa2601
Roope <roope.nahkala@gmail.com>
2023-11-29 10:57:34 +0200

    korjattu'D

--- client/src/xmlcomponents/News.js
+++ client/src/xmlcomponents/News.js
@@ -1,31 +1,57 @@
-const { default: axios } = require('axios');
-var xml2js = require('xml2js');
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import xml2js from 'xml2js';
 
-getXml();
+function NewsPage() {
+  const [newsData, setNewsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-//Promise version
-async function getXml(){
-    try {
+  useEffect(() => {
+    const getXml = async () => {
+      try {
+        // Replace with your actual news XML URL
         const result = await axios.get('https://www.finnkino.fi/xml/News/');
-
-        const jsResult = await xml2js.parseStringPromise(result.data);
-        
-        jsResult.NewsCategories.NewsArticleCategory.forEach(e => {
-            console.log(e.Name[0]);
+        const parser = new xml2js.Parser({
+          explicitArray: false,
+          // Adjust these options based on the news XML structure
+          explicitRoot: true,
+          mergeAttrs: true
         });
-        
-    } catch (error) {
-        console.log(error);
-    }
+
+        const jsResult = await parser.parseStringPromise(result.data);
+        // Adjust this line based on the structure of your news XML
+        const newsItems = jsResult.News.NewsItem;
+
+        setNewsData(Array.isArray(newsItems) ? newsItems : [newsItems]);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching or parsing news XML data:', error);
+        setLoading(false);
+      }
+    };
+
+    getXml();
+  }, []);
+
+  return (
+    <div>
+      <h1>News</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {newsData.map((item, index) => (
+            <li key={index}>
+              <strong>Title:</strong> {item.Title}
+              <br />
+              <strong>Description:</strong> {item.Description}
+              {/* Add other fields as needed */}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-
-// Callback version
-// axios.get('https://www.finnkino.fi/xml/News/')
-//     .then( res=> {
-//         parseString(res.data, (err, result) => {
-//             result.NewsCategories.NewsArticleCategory.forEach(e => {
-//                 console.log(e.Name[0]);
-//             });
-//         })
-//     });
+export default NewsPage;
--- client/src/xmlcomponents/TheatreAreas.js
+++ client/src/xmlcomponents/TheatreAreas.js
@@ -1,31 +1,62 @@
-const { default: axios } = require('axios');
-var xml2js = require('xml2js');
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import xml2js from 'xml2js';
 
-getXml();
+function TheatreAreasPage() {
+  const [xmlData, setXmlData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-//Promise version
-async function getXml(){
-    try {
-        const result = await axios.get('https://www.finnkino.fi/xml/TheatreAreas/');
+  useEffect(() => {
+    async function getXml() {
+        try {
+          const result = await axios.get('https://www.finnkino.fi/xml/TheatreAreas/');
+          const parser = new xml2js.Parser({
+            explicitArray: false,
+            // Try using explicitRoot: true or remove this line
+            explicitRoot: true,
+            mergeAttrs: true
+          });
+      
+          const jsResult = await parser.parseStringPromise(result.data);
+          console.log(jsResult); // Log the parsed result
+      
+          // Check if TheatreAreas and TheatreArea exist
+          if (jsResult.TheatreAreas && jsResult.TheatreAreas.TheatreArea) {
+            const theatreAreas = jsResult.TheatreAreas.TheatreArea;
+            setXmlData(Array.isArray(theatreAreas) ? theatreAreas : [theatreAreas]);
+          } else {
+            console.error('TheatreAreas or TheatreArea is not defined in the XML');
+            // Handle the case where the data is not in the expected format
+          }
+          setLoading(false);
+        } catch (error) {
+          console.error('Error fetching or parsing XML data:', error);
+          setLoading(false);
+        }
+      }
+      
 
-        const jsResult = await xml2js.parseStringPromise(result.data);
-        
-        jsResult.NewsCategories.NewsArticleCategory.forEach(e => {
-            console.log(e.Name[0]);
-        });
-        
-    } catch (error) {
-        console.log(error);
-    }
+    getXml();
+  }, []);
+
+  return (
+    <div>
+      <h1>Theatre Areas</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {xmlData.map((theatre, index) => (
+            <li key={index}>
+              <strong>ID:</strong> {theatre.ID}
+              <br />
+              <strong>Name:</strong> {theatre.Name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-
-// Callback version
-// axios.get('https://www.finnkino.fi/xml/TheatreAreas/')
-//     .then( res=> {
-//         parseString(res.data, (err, result) => {
-//             result.NewsCategories.NewsArticleCategory.forEach(e => {
-//                 console.log(e.Name[0]);
-//             });
-//         })
-//     });
+export default TheatreAreasPage;
