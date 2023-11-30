b5b65ed6b1dc4921e911f56ea892ec117de798c7
tompih <112546914+tompih@users.noreply.github.com>
2023-11-30 10:18:34 +0200

    update

--- client/src/xmlcomponents/Events.js
+++ client/src/xmlcomponents/Events.js
@@ -1,31 +1,58 @@
-const { default: axios } = require('axios');
-var xml2js = require('xml2js');
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import xml2js from 'xml2js';
+import './EventsPage.css'; // Import the CSS file
 
-getXml();
+function EventsPage() {
+  const [eventsData, setEventsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-//Promise version
-async function getXml(){
-    try {
+  useEffect(() => {
+    const getXml = async () => {
+      try {
         const result = await axios.get('https://www.finnkino.fi/xml/Events/');
-
-        const jsResult = await xml2js.parseStringPromise(result.data);
-        
-        jsResult.NewsCategories.NewsArticleCategory.forEach(e => {
-            console.log(e.Name[0]);
+        const parser = new xml2js.Parser({
+          explicitArray: false,
+          explicitRoot: true,
+          mergeAttrs: true
         });
-        
-    } catch (error) {
-        console.log(error);
-    }
+
+        const jsResult = await parser.parseStringPromise(result.data);
+        const events = jsResult.Events.Event;
+
+        setEventsData(Array.isArray(events) ? events : [events]);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching or parsing events XML data:', error);
+        setLoading(false);
+      }
+    };
+
+    getXml();
+  }, []);
+
+  return (
+    <div className="events-container">
+      <h1>Events</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="events-list">
+          {eventsData.map((item, index) => (
+            <li key={index} className="events-item">
+              <strong className="events-title">{item.Title || 'No title'}</strong>
+              <br />
+              <span className="events-description">{item.ShortSynopsis || 'No description'}</span>
+              <br />
+              {item.Images && item.Images.EventMediumImagePortrait && (
+                <img src={item.Images.EventMediumImagePortrait} alt={item.Title} className="events-image" />
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-
-// Callback version
-// axios.get('https://www.finnkino.fi/xml/Events/')
-//     .then( res=> {
-//         parseString(res.data, (err, result) => {
-//             result.NewsCategories.NewsArticleCategory.forEach(e => {
-//                 console.log(e.Name[0]);
-//             });
-//         })
-//     });
+export default EventsPage;
