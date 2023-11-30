8ed8603f853a7827357fff15ef29b28d0f7c3795
valtterivimpari <t3viva00@students.oamk.fi>
2023-11-30 09:57:22 +0200

    events update

--- client/src/xmlcomponents/Events.js
+++ client/src/xmlcomponents/Events.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import xml2js from 'xml2js';
-import './EventsPage.css'; // Import the CSS file
+import './EventsPage.css';
 
 function EventsPage() {
   const [eventsData, setEventsData] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchCriteria, setSearchCriteria] = useState({
+    productionYear: '',
+    rating: '',
+    genres: '', // Add genres to search criteria
+    globalDistributor: '', // Add global distributor to search criteria
+  });
 
   useEffect(() => {
     const getXml = async () => {
@@ -14,13 +21,14 @@ function EventsPage() {
         const parser = new xml2js.Parser({
           explicitArray: false,
           explicitRoot: true,
-          mergeAttrs: true
+          mergeAttrs: true,
         });
 
         const jsResult = await parser.parseStringPromise(result.data);
         const events = jsResult.Events.Event;
 
         setEventsData(Array.isArray(events) ? events : [events]);
+        setFilteredEvents(Array.isArray(events) ? events : [events]);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching or parsing events XML data:', error);
@@ -31,21 +39,94 @@ function EventsPage() {
     getXml();
   }, []);
 
+  const handleSearch = () => {
+    const filtered = eventsData.filter(
+      (event) =>
+        (searchCriteria.productionYear === '' ||
+          event.ProductionYear === searchCriteria.productionYear.toString()) &&
+        (searchCriteria.rating === '' || event.Rating === searchCriteria.rating) &&
+        (searchCriteria.genres === '' ||
+          event.Genres.toLowerCase().includes(searchCriteria.genres.toLowerCase())) &&
+        (searchCriteria.globalDistributor === '' ||
+          event.GlobalDistributorName
+            .toLowerCase()
+            .includes(searchCriteria.globalDistributor.toLowerCase()))
+    );
+
+    setFilteredEvents(filtered);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setSearchCriteria((prevCriteria) => ({ ...prevCriteria, [name]: value }));
+  };
+
+  const formatLocalReleaseDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
   return (
     <div className="events-container">
       <h1>Events</h1>
+
+      <div className="search-form">
+        <label>
+          Production Year:
+          <input
+            type="text"
+            name="productionYear"
+            value={searchCriteria.productionYear}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        <label>
+          Rating:
+          <input
+            type="text"
+            name="rating"
+            value={searchCriteria.rating}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        <label>
+          Genres:
+          <input
+            type="text"
+            name="genres"
+            value={searchCriteria.genres}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        <label>
+          Global Distributor:
+          <input
+            type="text"
+            name="globalDistributor"
+            value={searchCriteria.globalDistributor}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        <button onClick={handleSearch}>Search</button>
+      </div>
+
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ul className="events-list">
-          {eventsData.map((event, index) => (
-            <li key={index} className="event-item">
-              <strong className="event-title">{event.Title || 'No title'}</strong>
+        <div>
+          {filteredEvents.map((event, index) => (
+            <div key={index} className="event-item">
+              <h2 className="event-title">{event.Title || 'No title'}</h2>
               <p>
                 <strong>Production Year:</strong> {event.ProductionYear}
-              </p>
-              <p>
-                <strong>Length:</strong> {event.LengthInMinutes} minutes
               </p>
               <p>
                 <strong>Rating:</strong> {event.Rating}
@@ -54,7 +135,14 @@ function EventsPage() {
                 <strong>Genres:</strong> {event.Genres}
               </p>
               <p>
-                <strong>Synopsis:</strong> {event.Synopsis}
+                <strong>Synopsis:</strong> {event.ShortSynopsis}
+              </p>
+              <p>
+                <strong>Global Distributor:</strong> {event.GlobalDistributorName}
+              </p>
+              <p>
+                <strong>Local Release Date:</strong>{' '}
+                {formatLocalReleaseDate(event.dtLocalRelease)}
               </p>
               {event.Images && event.Images.EventMediumImagePortrait && (
                 <img
@@ -63,10 +151,10 @@ function EventsPage() {
                   className="event-image"
                 />
               )}
-              <br />
-            </li>
+              <hr />
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
