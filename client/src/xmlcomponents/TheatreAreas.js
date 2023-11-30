c285e5ff5cd3c636afb3f57b7a41798110c2c38b
tompih <112546914+tompih@users.noreply.github.com>
2023-11-30 13:34:53 +0200

    update

--- client/src/xmlcomponents/TheatreAreas.js
+++ client/src/xmlcomponents/TheatreAreas.js
@@ -1,6 +1,9 @@
+// TheatreAreas.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import xml2js from 'xml2js';
+import './TheatreAreasPage.css'; // Import the CSS file
 
 function TheatreAreasPage() {
   const [xmlData, setXmlData] = useState([]);
@@ -8,46 +11,43 @@ function TheatreAreasPage() {
 
   useEffect(() => {
     async function getXml() {
-        try {
-          const result = await axios.get('https://www.finnkino.fi/xml/TheatreAreas/');
-          const parser = new xml2js.Parser({
-            explicitArray: false,
-            // Try using explicitRoot: true or remove this line
-            explicitRoot: true,
-            mergeAttrs: true
-          });
-      
-          const jsResult = await parser.parseStringPromise(result.data);
-          console.log(jsResult); // Log the parsed result
-      
-          // Check if TheatreAreas and TheatreArea exist
-          if (jsResult.TheatreAreas && jsResult.TheatreAreas.TheatreArea) {
-            const theatreAreas = jsResult.TheatreAreas.TheatreArea;
-            setXmlData(Array.isArray(theatreAreas) ? theatreAreas : [theatreAreas]);
-          } else {
-            console.error('TheatreAreas or TheatreArea is not defined in the XML');
-            // Handle the case where the data is not in the expected format
-          }
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching or parsing XML data:', error);
-          setLoading(false);
+      try {
+        const result = await axios.get('https://www.finnkino.fi/xml/TheatreAreas/');
+        const parser = new xml2js.Parser({
+          explicitArray: false,
+          explicitRoot: true,
+          mergeAttrs: true,
+        });
+
+        const jsResult = await parser.parseStringPromise(result.data);
+
+        // Check if TheatreAreas and TheatreArea exist
+        if (jsResult.TheatreAreas && jsResult.TheatreAreas.TheatreArea) {
+          const theatreAreas = jsResult.TheatreAreas.TheatreArea;
+          setXmlData(Array.isArray(theatreAreas) ? theatreAreas : [theatreAreas]);
+        } else {
+          console.error('TheatreAreas or TheatreArea is not defined in the XML');
+          // Handle the case where the data is not in the expected format
         }
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching or parsing XML data:', error);
+        setLoading(false);
       }
-      
+    }
 
     getXml();
   }, []);
 
   return (
-    <div>
-      <h1>Theatre Areas</h1>
+    <div className="theatre-container">
+      <h1 className="theatre-title">Theatre Areas</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ul>
+        <ul className="theatre-list">
           {xmlData.map((theatre, index) => (
-            <li key={index}>
+            <li key={index} className="theatre-item">
               <strong>ID:</strong> {theatre.ID}
               <br />
               <strong>Name:</strong> {theatre.Name}
