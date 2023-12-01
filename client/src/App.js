73f4f86d1131b4bebf1b7be33e610d345fd57ba3
PohjanenJulius <julius.pohjanen@gmail.com>
2023-12-01 13:23:38 +0200

    review updates and responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -39,34 +39,26 @@ function App() {
   return (
     <AuthProvider>
       <RepeatingLogComponent />
-
-      <Router>
-        <TopBar /> {/* Render TopBar on all pages */}
-        <Routes>
-          <Route path="/" element={<HomePage />} /> {/* HomePage as the default route */}
-          <Route path="/movies/:movieId" element={<MovieDetail />} />
-          <Route path="/Auth" element={<Login />} /> {/* Login page */}
-          <Route path="/register" element={<Register />} /> {/* Registration page */}
-          <Route path="/profile" element={<Profile />} /> {/* Profile page */}
-          <Route path="/edit_profile" element={<Edit_Profile />} /> {/* Edit profile page */}
-          <Route path="/create_group" element={<CreateGroup />} /> {/* Create group page */}
-          <Route path="/edit_group/:groupId" element={<EditGroup />} /> {/* Edit group page */}
-          <Route path="/browse_all" element={<BrowseAll />} /> {/* Browse all page */}
-          <Route path="/browse_reviews" element={<BrowseReviews />} /> {/* Browse reviews page */}
-          <Route path="/search" element={<SearchPage />} /> {/* Search page */}
-          <Route path="/search_groups" element={<SearchGroups />} /> {/* Search group page */}
-          <Route path="/groups/:groupId" element={<GroupPage/>} />
-          <Route path="/theatre_areas" element={<TheatreAreasPage />} />
-          <Route path="/news" element={<NewsPage />} />
-
-          {/* <Route path="/xml2" element={<XmlPage xmlUrl="https://www.finnkino.fi/xml/ScheduleDates/" topLevelProperty="ScheduleDates" nestedProperty="ScheduleDate" />} />
-          <Route path="/xml3" element={<XmlPage xmlUrl="https://www.finnkino.fi/xml/Schedule/" topLevelProperty="Schedule" nestedProperty="Shows" />} />
-          <Route path="/xml4" element={<XmlPage xmlUrl="https://www.finnkino.fi/xml/Events/" topLevelProperty="Events" nestedProperty="Event" />} />
-          <Route path="/xml5" element={<XmlPage xmlUrl="https://www.finnkino.fi/xml/News/" topLevelProperty="News" nestedProperty="NewsArticle" />} />
-          <Route path="/xml6" element={<XmlPage xmlUrl="https://www.finnkino.fi/xml/NewsCategories/" topLevelProperty="NewsCategories" nestedProperty="NewsArticleCategory" />} /> */}
-        </Routes>
-      </Router>
-
+        <Router>
+          <TopBar /> {/* Render TopBar on all pages */}
+          <Routes>
+            <Route path="/" element={<HomePage />} /> {/* HomePage as the default route */}
+            <Route path="/movies/:movieId" element={<MovieDetail />} />
+            <Route path="/Auth" element={<Login />} /> {/* Login page */}
+            <Route path="/register" element={<Register />} /> {/* Registration page */}
+            <Route path="/profile" element={<Profile />} /> {/* Profile page */}
+            <Route path="/edit_profile" element={<Edit_Profile />} /> {/* Edit profile page */}
+            <Route path="/create_group" element={<CreateGroup />} /> {/* Create group page */}
+            <Route path="/edit_group/:groupId" element={<EditGroup />} /> {/* Edit group page */}
+            <Route path="/browse_all" element={<BrowseAll />} /> {/* Browse all page */}
+            <Route path="/browse_reviews" element={<BrowseReviews />} /> {/* Browse reviews page */}
+            <Route path="/search" element={<SearchPage />} /> {/* Search page */}
+            <Route path="/search_groups" element={<SearchGroups />} /> {/* Search group page */}
+            <Route path="/groups/:groupId" element={<GroupPage/>} />
+            <Route path="/theatre_areas" element={<TheatreAreasPage />} />
+            <Route path="/news" element={<NewsPage />} />
+          </Routes>
+        </Router>
     </AuthProvider>
   );
 }
